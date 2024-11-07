6ebfd73e46ba63b313fa9705bbd31b219e50c631
Nicolas Tolksdorf <tolksdorf@gmail.com>
2024-11-07 18:27:39 +0000

    Completed chapter 4 practice

--- Start/Ch_4/basiclog_start.py
+++ Start/Ch_4/basiclog_start.py
@@ -1,11 +1,21 @@
 # demonstrate the logging api in Python
+import logging
 
 # TODO: use the built-in logging module
 
 
 # TODO: Use basicConfig to configure logging
+logging.basicConfig(level=logging.DEBUG, 
+                    filemode='w', # overwrites log file
+                    filename='Start/Ch_4/output.log')
 
 # TODO: Try out each of the log levels
+logging.debug('Debug message')
+logging.info('Info message')
+logging.warning('Warning message')
+logging.error('Error message')
+logging.critical('Critical message')
+logging.critical('Critical var {} and int {}'.format(['Value'], 100)) # outputs: CRITICAL:root:Critical var ['Value'] and int 100
 
 # TODO: Output formatted strings to the log
 
--- Start/Ch_4/customlog_start.py
+++ Start/Ch_4/customlog_start.py
@@ -2,14 +2,24 @@
 
 import logging
 
-# TODO: add another function to log from
+# extraData = {} # KeyError: 'user'
+extraData = { "user": "jo@gmail.com" }
 
+# TODO: add another function to log from
+def another_function():
+    logging.debug("Another debugging message", extra=extraData)
 
 # set the output file and debug level, and
 # TODO: use a custom formatting specification
-logging.basicConfig(filename="output.log",
-                    level=logging.DEBUG)
+# time, level, func, line, user, message
+# fmtstr = '%(asctime)s: %(levelname)s: %(funcName)s Line:%(lineno)d User:%(user)s %(message)s'
+fmtstr = "%(asctime)s: %(levelname)s: %(funcName)s Line:%(lineno)d User:%(user)s %(message)s"
+datestr = '%m/%d/%Y %I:%M:%S %p'
+logging.basicConfig(filename="Start/Ch_4/output.log",
+                    level=logging.DEBUG, 
+                    format=fmtstr, 
+                    datefmt=datestr)
 
-logging.info("This is an info-level log message")
-logging.warning("This is a warning-level message")
-
+logging.info("This is an info-level log message", extra=extraData)
+logging.warning("This is a warning-level message", extra=extraData)
+another_function()
